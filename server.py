fa0b8613cc8d1311d1e8dcd000e33a4fc39e8925
Aung-Thet <aungth.htwe13@gmail.com>
2026-05-10 17:11:21 -0400

    Web Deployment

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request 
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+@app.route("/emotionDetector")
+
+def sent_emo():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+    dominant_emo = response.pop('dominant_emotion')
+    sys_response = ", ".join([f"{key}: {val}" for key, val in response.items()])
+
+    return f"For the given statement, the system response is {sys_response}. The dominant emotion is <b>{dominant_emo}</b>."
+
+@app.route("/")
+
+def render_index_page():
+    return render_template('index.html')
+
+if __name__=="__main__":
+    app.run(host = "0.0.0.0", port = 5000)
+
+
